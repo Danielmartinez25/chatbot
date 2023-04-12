96d59c363e9156af63a76d3251704cd8d2bdde8a
Danimartinez25 <danimartinez1325@gmail.com>
2023-04-12 01:58:52 -0300

    create public and init chat

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3525;
-
 // Convierte una petición recibida (POST-GET...) a objeto JSON
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.get("/", function (req, res) {
-  res.status(200).send({
-    message: "GET Home route working fine!",
-  });
-});
-
-app.listen(port, function () {
+app.use(express.static(path.join(__dirname, "public")));
+const server = app.listen(port, function () {
   console.log(`Server running in http://localhost:${port}`);
-  console.log("Defined routes:");
-  console.log("	[GET] http://localhost:3525/");
 });
+const SocketIO = require('socket.io')
+const io = SocketIO(server)
+//webSockets
+io.on('connection', () => {
+    console.log('new connection')
+})
